a14dbf6bb3599880d10905dc3b3fc84f032574a6
Anastasia Marie <ana.goodwin10@gmail.com>
2017-03-30 14:33:42 -0500

    updated ajax call

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+
+
+var queryURL= "http://api.petfinder.com/pet.find?format=json&key=96d7e760e6cf087c0470a585636831ff&location=78723&animal=dog"
+
+
+
+$.ajax({
+          url: queryURL,
+          method: "GET"
+        })
+.done(function(response) {
+          console.log(queryURL);
+
+          console.log(response);
+          // storing the data from the AJAX request in the results variable
+          var results = response.response;
+      });
