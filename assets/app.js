26041c657773f1b63c082119a80ffdb26fcdd0ff
Your Name <s.nguyen11@utexas.edu>
2017-03-31 12:50:38 -0500

    error message alert

--- assets/app.js
+++ assets/app.js
@@ -1,60 +1,64 @@
+$(document).ready(function() {
+  $('select').material_select();
 
- $(document).ready(function() {
-    $('select').material_select();
-
-$("#submit").click(function(event){
+  $("#submit").click(function(event){
 	event.preventDefault();
-var breed = $('#breed').val();
-console.log(breed);
-var age = $("#age").val();
-console.log(age);
-var size = $("#size").val();
-console.log(size);
-var gender = $("#gender").val();
-console.log(gender);
-var zipcode = $("#location").val();
-console.log(zipcode);
-var api_key = '96d7e760e6cf087c0470a585636831ff';
+	var breed = $('#breed').val();
+	console.log(breed);
+	var age = $("#age").val();
+	console.log(age);
+	var size = $("#size").val();
+	console.log(size);
+	var gender = $("#gender").val();
+	console.log(gender);
+	var zipcode = $("#location").val();
+	console.log(zipcode);
+	var api_key = '96d7e760e6cf087c0470a585636831ff';
+	var queryURL= "http://api.petfinder.com/pet.find?";
 
+	if(zipcode === ""){
+		$("#error").text("Location is required.");
+	}
 
-var queryURL= "http://api.petfinder.com/pet.find?";
-
-
-queryURL += $.param({
-	// 'breed': breed,
-	'format': 'json',
-	'key': api_key,
-	'animal': "dog",
-	'sex': gender,
-	'location': zipcode, 
-	'age': age,
-
-	'size': size 
+	queryURL += $.param({
+	  'breed': breed,
+	  'format': 'json',
+	  'key': api_key,
+	  'animal': "dog",
+	  'sex': gender,
+	  'location': zipcode, 
+	  'age': age,
+	  'size': size 
+	});
 	
-	 
-});
-$.ajax({
-          url: queryURL,
-          method: "GET",
-          crossDomain: true,
-          dataType: "jsonp"
-        })
-.done(function(response) {
-          console.log(queryURL);
+	$.ajax({
+	  url: queryURL,
+	  method: "GET",
+	  crossDomain: true,
+	  dataType: "jsonp",
 
+    }).done(function(response) {
+ 		if(response.petfinder.pets){
+  		  console.log(queryURL);
           console.log(response);
           console.log(response.petfinder.pets.pet);
           var theArrayOfNope = response.petfinder.pets.pet;
           theArrayOfNope.forEach(function(currentPet){
-          	console.log(currentPet.name.$t);
-          });
-          // storing the data from the AJAX request in the results variable
-          var results = response.response;
-      });
-});
+			console.log(currentPet.name.$t); 
 
- 
+      	  });
+	  	  // storing the data from the AJAX request in the results variable
+          var results = response.response;
+ 		}else{
+ 			$("#error").text("No results found, try again.");
+ 		}
+  
+      });
+    
+
+
   });
+});
 
 
 
