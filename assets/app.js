2c69642b259d14c842b88d02efa0e1da655477d7
Your Name <s.nguyen11@utexas.edu>
2017-03-31 10:38:28 -0500

    js

--- assets/app.js
+++ assets/app.js
@@ -1,54 +1,56 @@
-$("#submit").on("click", function(){
-var breed = $('#breed').val();
-console.log(breed);
-var age = $("#age").val();
-console.log(age);
-var size = $("#size").val();
-console.log(size);
-var gender = $("#gender").val();
-console.log(gender);
-var zipcode = $("#location").val();
-console.log(zipcode);
-var api_key = '96d7e760e6cf087c0470a585636831ff';
-});
- 
+$(document).ready(function() {
+  $('select').material_select();
 
-var queryURL= "http://api.petfinder.com/pet.find?";
-
-
-queryURL += $.param({
-	// 'breed': breed,
-	'format': 'json',
-	'key': api_key,
-	'animal': "dog",
-	'sex': "F",
-	'location': zipcode, 
-	'age': "adult",
-
-	'size': "M"
+	$("#submit").on("click", function(){
+	var breed = $('#breed').val();
+	console.log(breed);
+	var age = $("#age").val();
+	console.log(age);
+	var size = $("#size").val();
+	console.log(size);
+	var gender = $("#gender").val();
+	console.log(gender);
+	var zipcode = $("#location").val();
+	console.log(zipcode);
 	
+	});
 	 
+  var api_key = '96d7e760e6cf087c0470a585636831ff';
+  var queryURL= "http://api.petfinder.com/pet.find?";
+
+
+	queryURL += $.param({
+		// 'breed': breed,
+		'format': 'json',
+		'key': api_key,
+		'animal': "dog",
+		'sex': "F",
+		'location': zipcode, 
+		'age': "adult",
+
+		'size': "M"
+		
+		 
+	});
+	$.ajax({
+	          url: queryURL,
+	          method: "GET",
+	          crossDomain: true,
+	          dataType: "jsonp"
+	        })
+	.done(function(response) {
+	          console.log(queryURL);
+
+	          console.log(response);
+	          console.log(response.petfinder.pets.pet);
+	          var theArrayOfNope = response.petfinder.pets.pet;
+	          theArrayOfNope.forEach(function(currentPet){
+	          	console.log(currentPet.name.$t);
+	          });
+	          // storing the data from the AJAX request in the results variable
+	          var results = response.response;
+	      });
+
+
 });
-$.ajax({
-          url: queryURL,
-          method: "GET",
-          crossDomain: true,
-          dataType: "jsonp"
-        })
-.done(function(response) {
-          console.log(queryURL);
-
-          console.log(response);
-          console.log(response.petfinder.pets.pet);
-          var theArrayOfNope = response.petfinder.pets.pet;
-          theArrayOfNope.forEach(function(currentPet){
-          	console.log(currentPet.name.$t);
-          });
-          // storing the data from the AJAX request in the results variable
-          var results = response.response;
-      });
-
-  $(document).ready(function() {
-    $('select').material_select();
-  });
 
