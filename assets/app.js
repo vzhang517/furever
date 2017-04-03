be9d54ae8b871692cac15b83e4ff53500be50a9c
Your Name <s.nguyen11@utexas.edu>
2017-04-03 10:21:21 -0500

    remove comment

--- assets/app.js
+++ assets/app.js
@@ -168,7 +168,7 @@ $(document).ready(function() {
 					$("#cards").append("<div class='card sticky-action col s4'><div class='card-image waves-effect waves-block waves-light'><img class='activator' src='"+dogPicArray[0]+"'></div><div class='card-content'><span class='card-title activator grey-text text-darken-4'>"+dogName+"</span><p>"+dogAge+" // "+dogSize+"</p></div><div class='card-action'><span><a class='waves-effect waves-teal btn-flat' id='like'>Like</a><a class='waves-effect waves-teal btn-flat' id='no'>Not for Me</a></span></div><div class='card-reveal'><span class='card-title grey-text text-darken-4'>"+dogName+"</span><p>"+addressOne+"<br>"+city+", "+state+" "+zip+"<br>"+email+"</p></div></div>")
 					}
 					add();
-					// width=10&-x.jpg 
+				
                 });
 
             	$("#search").css("display", "none");
