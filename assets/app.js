e4d8014a1b2d019a96bff6732fe8f2d104b6ba85
Brendan Rogers <brendan@utexas.edu>
2017-04-02 20:58:33 -0500

    added comments to js ammendments

--- assets/app.js
+++ assets/app.js
@@ -97,7 +97,7 @@ $(document).ready(function() {
                     console.log("Dog Pic Array: " + JSON.stringify(dogPicArray));
 
 
-                    //////////////// Contact info pulled here and assigned to relevant variable if key ($t) exists/////////    
+                    //////////////// Contact info pulled here (under forEach function) and assigned to relevant variable if key ($t) exists/////////    
                     /////////////////// If key does not exist then variable is assigned a "No ... provided" message////
 
                     if (currentPet.contact.address1.hasOwnProperty('$t')) {
